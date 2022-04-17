27a7a3e823e7c3c719095d0b4801ddeca52e6cd0
melisadeniz <melisadeniz95@gmail.com>
2022-04-17 23:32:43 +0300

    first commit

---
+++ src/containers/App/index.js
@@ -0,0 +1,40 @@
+// @flow
+import React from "react";
+import styled from "styled-components";
+import Home from "../Home";
+
+const Container = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+  align-items: center;
+  height: 100vh;
+  width: 98%;
+  padding-top: 30px ;
+  font-family: Montserrat;
+`;
+
+const Header = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 22px;
+  font-size: 25px;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+function App() {
+  return (
+    <Container>
+    <Header>Expense Tracker</Header>
+    <Home />
+  </Container>
+  );
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+// import Budget from "../containers/Budget";
+// import Reports from "../containers/Reports";
+
+// const routes = [
+//   { title: "Budget", path: "/budget", element: Budget },
+//   { title: "Reports", path: "/reports", element: Reports },
+// ];
+
+// export default routes;
